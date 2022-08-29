dc42dba0706ed77c170bb574cb82144ed98da282
Kashyap Nirmal <kashyap.n@knackroot.com>
2022-08-29 14:40:48 +0530

    Map exercise.

---
+++ Go Tour/More_Types/exercise_map_23_27.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	words := strings.Fields(s)
+	// Word Frequency Count
+	for _, v := range words {
+		if _, ok := m[v]; ok == true {
+			m[v] += 1
+		} else {
+			m[v] = 1
+		}
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
+
+/*
+	O/P :
+
+		PASS
+		f("I am learning Go!") =
+		map[string]int{"Go!":1, "I":1, "am":1, "learning":1}
+		PASS
+		f("The quick brown fox jumped over the lazy dog.") =
+		map[string]int{"The":1, "brown":1, "dog.":1, "fox":1, "jumped":1, "lazy":1, "over":1, "quick":1, "the":1}
+		PASS
+		f("I ate a donut. Then I ate another donut.") =
+		map[string]int{"I":2, "Then":1, "a":1, "another":1, "ate":2, "donut.":2}
+		PASS
+		f("A man a plan a canal panama.") =
+		map[string]int{"A":1, "a":2, "canal":1, "man":1, "panama.":1, "plan":1}
+
+*/
